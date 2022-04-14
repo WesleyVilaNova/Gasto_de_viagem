c9ebbf2bc3c288f7a81f719746e8ae21fa258acb
Wesley Vila Nova <wesleyvilanovadelimatorres@gmail.com>
2022-04-14 13:47:43 -0300

    Lógica do negócio

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.gastodeviagem
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gastodeviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,6 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnCalcular.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_calcular) {
+            if (binding.editDistance.length() == 0){
+                binding.editDistance.error = "O campo não pode ficar vazio"
+            }else if (binding.editPrice.length() == 0){
+                binding.editPrice.error = "O campo não pode ficar vazio"
+            }else if (binding.editAutonomy.length() == 0){
+                binding.editAutonomy.error = "O campo não pode ficar vazio"
+            }else {
+                calcular()
+                binding.editDistance.setText("")
+                binding.editPrice.setText("")
+                binding.editAutonomy.setText("")
+
+            }
+        }
+    }
+
+    private fun calcular() {
+        val distance = binding.editDistance.text.toString().toDouble()
+        val price = binding.editPrice.text.toString().toDouble()
+        val autonomy = binding.editAutonomy.text.toString().toDouble()
+
+        val totalValue = (distance * price) / autonomy
+        val totalValueStr = "R$" + "%.2f".format(totalValue)
+        binding.textTotalValue.text = totalValueStr
+        Toast.makeText(this, "Calculo realizado com sucesso",Toast.LENGTH_SHORT).show()
 
     }
 
